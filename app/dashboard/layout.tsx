cdcd3b06d7dd1e99d619623cd651ec844dad7217
licairen <li51871314@163.com>
2024-12-12 14:06:48 +0800

    feat: 增加redis

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,10 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 // export const experimental_ppr = true;
-export default function Layout({ children }: { children: React.ReactNode }) {
+// import { redis } from '@/app/lib/redis'
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  // const data = await redis.get('foo')
+  // console.log(data, 'redis =====data');
+  
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
