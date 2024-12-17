c1bf07cb63f8b1f0399b0da3d39f52b3e94dbed0
lijianpeng <7737841@qq.com>
2024-12-17 10:13:15 +0800

    feat: 增加postinstall

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@/app/ui/global.css'
 import { Inter } from 'next/font/google'
 import 'prismjs/themes/prism-tomorrow.css'
 
-import { prisma } from '@/lib/prisma'
+// import { prissma } from '@/lib/prisma'
 
 // 初始化 Inter 字体
 const inter = Inter({ subsets: ['latin'] })
@@ -13,8 +13,8 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
 
-  const customers = await prisma.customers.findMany()
-  console.log(customers, 99999)
+  // const customers = await prisma.customers.findMany()
+  // console.log(customers, 99999)
 
   return (
     <html lang="en" className={inter.className}>
